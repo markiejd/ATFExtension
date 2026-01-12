392bee04a57c30e7054c4aca6daab862bd236528
markiejd <64020837+markiejd@users.noreply.github.com>
2026-01-12 14:27:51 +0000

    New output (#3)
    * New Version
    
    * Fix Formatting

--- src/extension.ts
+++ src/extension.ts
@@ -235,14 +235,15 @@ function generateCSharpMethod(stepLine: string): string {
     `${bindingAttribute ? `${bindingAttribute}\r\n` : ''}` +  // Attribute (if valid)
     `${signature}\r\n` +                                        // Method signature
     `{\r\n` +                                                   // Opening brace
-    `    ${buildProcAssignment(stepLine, parameterNames)}\r\n` + // Proc string assignment
-    `\r\n` +
-    `    if (CombinedSteps.OutputProc(proc))\r\n` +            // Call to framework
-    `    {\r\n` +
-    `        return false;\r\n` +
-    `    }\r\n` +
-    `\r\n` +
-    `    return false;\r\n` +
+    `   ${buildProcAssignment(stepLine, parameterNames)}\r\n` + // Proc string assignment
+    `    \r\n` +
+    `   if (CombinedSteps.OutputProc(proc))\r\n` +            // Call to framework
+    `   {\r\n` +
+    `     return false;\r\n  // make me true!` +
+    `     //return true;\r\n` +
+    `   }\r\n` +
+    `   CombinedSteps.Failure(proc);\r\n` +
+    `   return false;\r\n` +
     `}`
   );
 }
